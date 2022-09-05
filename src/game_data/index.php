65e8b6647ef1b191b6060c3f7aa88cc1a993507c
Roberto M. Oliva <floyd303@gmail.com>
2022-09-05 17:37:34 +0200

    Changed the main HTML

--- src/game_data/index.php
+++ src/game_data/index.php
@@ -23,14 +23,20 @@ $data = array(
 <div class="wrap">
   <h2><?php _e( 'Resistencia Lúdica - Game Data', 'rl_events_game_dara' ) ?></h2>
 </div>
-<div id="game_data">
-  <?php
-    foreach ($data['month_years'] as $monthYear) {
-      echo('<a href="/wp-admin/admin.php?page=rl_events%2Fsrc%2Fgame_data%2Findex.php&month_year='.$monthYear.'">'.$monthYear.'</a><br />');
-    }
-  ?>
-</div>
-
+<section id="game_data" class="game-data js__game-data">
+  <div class="game-data__main js__game-data-main"></div>
+  <aside class="game-data__default-nav game-data__default-nav--hidden js__game-data-default-nav" aria-hidden="true">
+    <ul class="game-data__default-nav-items js_game-data-default-nav-items">
+      <?php
+        foreach ($data['month_years'] as $monthYear) {
+          echo('<li class="game-data__default-nav-item">');
+          echo('<a class="game-data__default-nav-link js__game-data-default-nav-link" href="/wp-admin/admin.php?page=rl_events%2Fsrc%2Fgame_data%2Findex.php&month_year='.$monthYear.'">'.$monthYear.'</a><br />');
+          echo('</li>');
+        }
+      ?>
+    </ul>
+  </aside>
+</section>
 
 <script type="text/javascript">
 var game_data = <?php echo(json_encode($data, JSON_PRETTY_PRINT)); ?>;

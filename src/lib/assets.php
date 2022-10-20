b100812be51108ac4fdb114a7c8c4a36376240ea
Roberto M. Oliva <floyd303@gmail.com>
2022-10-20 17:58:55 +0200

    Added the JavaScript UI developed by Chuck!

--- src/lib/assets.php
+++ src/lib/assets.php
@@ -8,6 +8,7 @@ add_action('admin_enqueue_scripts', 'rl_events_js');
 function rl_events_styles() {
   wp_enqueue_style('rl_events', plugins_url('../../assets/stylesheets/rl_events.css', __FILE__), false, NULL, 'all' );
   wp_enqueue_style('datepicker', plugins_url('../../assets/stylesheets/mc-calendar.min.css', __FILE__), false, NULL, 'all' );
+  wp_enqueue_style('game_data', plugins_url('../../assets/stylesheets/game_data/game-data.css', __FILE__), false, NULL, 'all' );
 }
 
 function rl_events_js() {
@@ -15,4 +16,5 @@ function rl_events_js() {
   wp_enqueue_script( 'datepicker', plugins_url('../../assets/javascripts/mc-calendar.min.js', __FILE__), false, NULL, false );
   wp_enqueue_script( 'jquery_validate', plugins_url('../../assets/javascripts/jquery.validate.min.js', __FILE__), false, NULL, false );
   wp_enqueue_script( 'jquery_validate_additional', plugins_url('../../assets/javascripts/additional-methods.min.js', __FILE__), false, NULL, false );
+  wp_enqueue_script( 'game_data', plugins_url('../../assets/javascripts/game-data.js', __FILE__), false, NULL, false );
 }
